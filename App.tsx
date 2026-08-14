8486d94a7fc2f30d216686340e8b24f106aefa49
sachinghimire0987-skyrise <sachin.ghimire0987@gmail.com>
2026-08-14 21:43:02 +0545

    Replace welcome message with Index component

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SiteLayout from '@/layouts/SiteLayout';
-import ProtectedRoute from '@/routes/ProtectedRoute';
+import Index from '@/pages/Index'; // वा तपाईंको Landing Page Component
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<SiteLayout />}>
-          <Route index element={<div className="p-8 font-sans">Welcome to Sachin Ghimire Site</div>} />
+          <Route index element={<Index />} />
         </Route>
       </Routes>
     </BrowserRouter>
