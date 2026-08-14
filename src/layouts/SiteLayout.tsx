4fe1f757d42daed861eca250a3da5a1744160b5f
sachinghimire0987-skyrise <sachin.ghimire0987@gmail.com>
2026-08-14 21:41:18 +0545

    Refactor SiteLayout to use React and Outlet

--- src/layouts/SiteLayout.tsx
+++ src/layouts/SiteLayout.tsx
@@ -1,1 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+export function SiteLayout() {
+  return (
+    <div className="min-h-screen bg-background font-sans antialiased">
+      <main className="flex-1">
+        <Outlet />
+      </main>
+    </div>
+  );
+}
+
 export default SiteLayout;
